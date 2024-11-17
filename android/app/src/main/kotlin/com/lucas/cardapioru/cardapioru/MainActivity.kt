fe3f01f7b6f5eb05638bb89f48a814d805792f5d
nsllongo <lucaslongor1@gmail.com>
2024-11-16 21:06:13 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/lucas/cardapioru/cardapioru/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lucas.cardapioru.cardapioru
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:cardapioru/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
